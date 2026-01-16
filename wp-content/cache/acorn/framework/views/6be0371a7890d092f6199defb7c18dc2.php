6abe9dde65587fc88af418ebaf895f6fea46abbe
Danny Spangenberg <danny.spangenberg@twiceware.de>
2026-01-16 16:51:38 +0100

    first commit

--- wp-content/cache/acorn/framework/views/6be0371a7890d092f6199defb7c18dc2.php
+++ wp-content/cache/acorn/framework/views/6be0371a7890d092f6199defb7c18dc2.php
@@ -13,12 +13,26 @@
     <img src="<?php echo e(get_theme_file_uri('resources/images/steinermuehle-de-logo.png')); ?>"  alt="Logo Steinermühle " class="h-12 shadow border rounded-full mx-2">
   </a>
 
-  <?php if(has_nav_menu('primary_navigation')): ?>
-    <nav class="nav-primary" aria-label="<?php echo e(wp_get_nav_menu_name('primary_navigation')); ?>">
-      <?php echo wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]); ?>
-
-    </nav>
-  <?php endif; ?>
+  <?php if (isset($component)) { $__componentOriginalf75d29720390c8e1fa3307604849a543 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginalf75d29720390c8e1fa3307604849a543 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.navigation','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('navigation'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginalf75d29720390c8e1fa3307604849a543)): ?>
+<?php $attributes = $__attributesOriginalf75d29720390c8e1fa3307604849a543; ?>
+<?php unset($__attributesOriginalf75d29720390c8e1fa3307604849a543); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalf75d29720390c8e1fa3307604849a543)): ?>
+<?php $component = $__componentOriginalf75d29720390c8e1fa3307604849a543; ?>
+<?php unset($__componentOriginalf75d29720390c8e1fa3307604849a543); ?>
+<?php endif; ?>
  <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__attributesOriginala766c2d312d6f7864fe218e2500d2bba)): ?>
---
+++ wp-content/themes/steinermuehle.de/resources/views/components/navigation.blade.php
@@ -0,0 +1,196 @@
+<nav x-data="{
+navigationMenuOpen: false,
+  navigationMenu: '',
+  navigationMenuCloseDelay: 200,
+  navigationMenuCloseTimeout: null,
+  navigationMenuLeave() {
+  let that = this;
+  this.navigationMenuCloseTimeout = setTimeout(() => {
+    that.navigationMenuClose();
+  }, this.navigationMenuCloseDelay);
+},
+navigationMenuReposition(navElement) {
+  this.navigationMenuClearCloseTimeout();
+  this.$refs.navigationDropdown.style.left = navElement.offsetLeft + 'px';
+  this.$refs.navigationDropdown.style.marginLeft = (navElement.offsetWidth/2) + 'px';
+},
+navigationMenuClearCloseTimeout(){
+  clearTimeout(this.navigationMenuCloseTimeout);
+},
+navigationMenuClose(){
+  this.navigationMenuOpen = false;
+  this.navigationMenu = '';
+}
+}"
+     class="relative z-50 w-auto mx-auto hidden md:flex text-left"
+>
+    <div class="relative">
+        <ul
+                class="flex flex-1 uppercase justify-center items-center p-1 space-x-1 list-none rounded-md text-neutral-700 group border-neutral-200/80"
+        >
+            <li>
+                <button
+                        :class="{ 'bg-neutral-100' : navigationMenu=='getting-started', 'hover:bg-neutral-100' : navigationMenu!='getting-started' }"
+                        @mouseover="navigationMenuOpen=true; navigationMenuReposition($el); navigationMenu='getting-started'"
+                        @mouseleave="navigationMenuLeave()"
+                        class="inline-flex justify-center items-center uppercase px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none group"
+                >
+                    <span>Die Steinermühle</span>
+                    <svg :class="{ '-rotate-180' : navigationMenuOpen==true && navigationMenu == 'getting-started' }"
+                         class="relative top-[1px] ml-1 h-3 w-3 ease-out duration-300" xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" aria-hidden="true"
+                    >
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <button
+                        :class="{ 'bg-neutral-100' : navigationMenu=='erlebnis', 'hover:bg-neutral-100' : navigationMenu!='erlebnis' }"
+                        @mouseover="navigationMenuOpen=true; navigationMenuReposition($el); navigationMenu='erlebnis'"
+                        @mouseleave="navigationMenuLeave()"
+                        class="inline-flex justify-center items-center uppercase px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none group"
+                >
+                    <span>Erlebnis Steinermühle</span>
+                    <svg :class="{ '-rotate-180' : navigationMenuOpen==true && navigationMenu == 'getting-started' }"
+                         class="relative top-[1px] ml-1 h-3 w-3 ease-out duration-300" xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" aria-hidden="true"
+                    >
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <button
+                        :class="{ 'bg-neutral-100' : navigationMenu=='learn-more', 'hover:bg-neutral-100' : navigationMenu!='learn-more' }"
+                        @mouseover="navigationMenuOpen=true; navigationMenuReposition($el); navigationMenu='learn-more'"
+                        @mouseleave="navigationMenuLeave()"
+                        class="inline-flex justify-center uppercase items-center px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-neutral-100 group"
+                >
+                    <span>Ferienwohnungen</span>
+                    <svg :class="{ '-rotate-180' : navigationMenuOpen==true && navigationMenu == 'learn-more' }"
+                         class="relative top-[1px] ml-1 h-3 w-3 ease-out duration-300" xmlns="http://www.w3.org/2000/svg"
+                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                         stroke-linejoin="round" aria-hidden="true"
+                    >
+                        <polyline points="6 9 12 15 18 9"></polyline>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <a href="#_"
+                   class="inline-flex justify-center items-center px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-neutral-100 group"
+                >
+                    Umgebung
+                </a>
+            </li>
+            <li>
+                <a href="#_"
+                   class="inline-flex justify-center items-center px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-neutral-100 group"
+                >
+                    Gästestimmen
+                </a>
+            </li>
+            <li>
+                <a href="#_"
+                   class="inline-flex justify-center items-center px-4 py-2 w-max h-10 text-lg font-medium rounded-md transition-colors hover:text-neutral-900 focus:outline-none disabled:opacity-50 disabled:pointer-events-none bg-background hover:bg-neutral-100 group"
+                >
+                    Anfahrt + Kontakt
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div x-ref="navigationDropdown" x-show="navigationMenuOpen"
+         x-transition:enter="transition ease-out duration-100"
+         x-transition:enter-start="opacity-0 scale-90"
+         x-transition:enter-end="opacity-100 scale-100"
+         x-transition:leave="transition ease-in duration-100"
+         x-transition:leave-start="opacity-100 scale-100"
+         x-transition:leave-end="opacity-0 scale-90"
+         @mouseover="navigationMenuClearCloseTimeout()" @mouseleave="navigationMenuLeave()"
+         :class="{ 'left-1/2 -translate-x-1/2 w-screen max-w-7xl' : navigationMenu == 'learn-more' }"
+         class="absolute top-0 pt-3 duration-200 ease-out -translate-x-1/2 translate-y-11" x-cloak
+    >
+
+        <div
+                :class="{ 'w-full max-w-none' : navigationMenu == 'learn-more', 'w-auto' : navigationMenu != 'learn-more' }"
+                class="flex overflow-hidden justify-center h-auto bg-white rounded-md border shadow-sm border-neutral-200/70"
+        >
+
+            <div x-show="navigationMenu == 'getting-started'"
+                 class="flex gap-x-3 justify-center items-stretch p-6 w-full max-w-2xl"
+            >
+                <div class="flex-shrink-0 pb-7 w-48 h-auto bg-white">
+
+                    <img src="/Quadra_Mockup_Steinermuehle.jpg" />
+                </div>
+                <div class="w-72">
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Geschichte + Buch</span>
+                        <span class="block font-light leading-5 opacity-70">Die Geschichte der Steinermühle und das Buch zur Mühle.</span>
+                    </a>
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Ahnengalerie</span>
+                        <span class="block leading-5 opacity-50"
+                        >Tiere, die mal in der Mühle gelebt haben.</span>
+                    </a>
+                </div>
+            </div>
+            <div x-show="navigationMenu == 'erlebnis'" class="flex justify-center items-stretch p-6 w-full">
+                <div class="w-96">
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Urlaub von Anfang an</span>
+                        <span class="block font-light leading-5 opacity-50">Ankommen und die Seele baumeln lassen.</span>
+                    </a>
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Ferien mit Kind</span>
+                        <span class="block font-light leading-5 opacity-50">The perfect all-in-one framework for building amazing apps.</span>
+                    </a>
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Ferien mit Hund</span>
+                        <span class="block leading-5 opacity-50"
+                        >An Alpine JS and Tailwind CSS UI library for awesome people. </span>
+                    </a>
+
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Seminare + mehr</span>
+                        <span class="block leading-5 opacity-50"
+                        >Seminare, Teambuilding und Familentreffen</span>
+                    </a>
+                    <a href="#_" @click="navigationMenuClose()" class="block px-3.5 py-3 text-sm rounded hover:bg-neutral-100">
+                        <span class="block mb-1 font-medium text-black">Feiertags-Specials</span>
+                        <span class="block leading-5 opacity-50"
+                        >Ostern und Weihnachten in der Steinermühle</span>
+                    </a>
+                </div>
+            </div>
+            <div x-show="navigationMenu == 'learn-more'" class="flex flex-col justify-center items-stretch p-6 w-full">
+
+
+                        @query([
+                        'post_type' => 'page',
+                        'post_parent' => 10,
+                        'posts_per_page' => -1,
+                        'orderby' => 'menu_order',
+                        'order' => 'ASC'
+                        ])
+
+                        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-4">
+                            @posts
+                            <x-accommodation-card
+                                    :image="get_the_post_thumbnail_url(get_the_ID(), 'full')"
+                                    :url="get_permalink()"
+                                    :title="get_the_title()"
+                                    :beds="get_field('number_of_beds')"
+                                    :size="get_field('size')"
+                                    :stars="get_field('number_of_statrs')"
+                            />
+                            @endposts
+
+            </div>
+
+        </div>
+</nav>
+<?php
